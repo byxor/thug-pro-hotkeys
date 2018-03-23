3e8dd843d7577b8bed5e48dadec83638f900dd60
byxor <brandnewbyxor@gmail.com>
2018-03-23 23:42:49 +0000

    Move curly braces

--- Program.cs
+++ Program.cs
@@ -4,21 +4,17 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Diagnostics;
 
-namespace tprosetgoto
-{
-    class Program
-    {
+namespace tprosetgoto {
+    class Program {
         static int hwnd = 0;
         private const int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
         private static LowLevelKeyboardProc _proc = HookCallback;
         private static IntPtr _hookID = IntPtr.Zero;
 
-        private static IntPtr SetHook(LowLevelKeyboardProc proc)
-        {
+        private static IntPtr SetHook(LowLevelKeyboardProc proc) {
             using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
-            {
+            using (ProcessModule curModule = curProcess.MainModule) {
                 return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
             }
         }
@@ -51,19 +47,16 @@ namespace tprosetgoto
         const int WM_CHAR = 0x0102;
         const int VK_ENTER = 0x0D;
 
-        public static void pressEnter(int hwnd)
-        {
+        public static void pressEnter(int hwnd) {
             PostMessage((IntPtr)hwnd, WM_KEYDOWN, (IntPtr)VK_ENTER, 0);
         }
 
-        public static void pressChar(int hwnd, char c)
-        {
+        public static void pressChar(int hwnd, char c) {
             Thread.Sleep(10);
             PostMessage((IntPtr)hwnd, WM_CHAR, new IntPtr((Int32)c), 0);
         }
 
-        public static void postCommand(int hwnd, string msg)
-        {
+        public static void postCommand(int hwnd, string msg) {
             pressEnter(hwnd);
             Thread.Sleep(5);
             foreach (char c in msg)
@@ -72,13 +65,10 @@ namespace tprosetgoto
             pressEnter(hwnd);
         }
 
-        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
-            {
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
                 int vkCode = Marshal.ReadInt32(lParam);
-                if (hwnd != 0)
-                {
+                if (hwnd != 0) {
                     if (vkCode == 116) //F5
                         postCommand(hwnd, "/set");
                     else if (vkCode == 117) //F6
@@ -92,11 +82,9 @@ namespace tprosetgoto
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             hwnd = (int)FindWindow(null, progname);
-            if (hwnd == 0)
-            {
+            if (hwnd == 0) {
                 Console.WriteLine("THUGPro is not running");
                 return;
             }
@@ -105,6 +93,5 @@ namespace tprosetgoto
             Application.Run();
             UnhookWindowsHookEx(_hookID);
         }
-        
     }
 }
