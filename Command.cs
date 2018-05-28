b7dc862bd5231e5f4c7674918bfa9c0313878fb4
byxor <brandnewbyxor@gmail.com>
2018-05-28 23:00:36 +0100

    Move KeyCodes

---
+++ Command.cs
@@ -0,0 +1,33 @@
+namespace ThugPro {
+
+    class Command {
+        public static void Post(int windowHandle, string message) {
+            ToggleChatBox(windowHandle);
+            TypeMessage(windowHandle, message);
+            ToggleChatBox(windowHandle);
+        }
+
+        private static void ToggleChatBox(int windowHandle) {
+            PressEnter(windowHandle);
+            Thread.Sleep(Timing.CHATBOX_WAIT_MILLISECONDS);
+        }
+
+        private static void PressEnter(int windowHandle) {
+            PostMessage((IntPtr) windowHandle, KeyCodeTypes.WM_KEYDOWN, (IntPtr) KeyCodes.Get("Enter"), 0);
+        }
+
+        private static void TypeMessage(int windowHandle, string message) {
+            foreach (char c in message)
+                PressChar(windowHandle, c);
+        }
+
+        private static void PressChar(int windowHandle, char c) {
+            Thread.Sleep(Timing.CHARACTER_WAIT_MILLISECONDS);
+            PostMessage((IntPtr) windowHandle, KeyCodeTypes.WM_CHAR, new IntPtr((Int32) c), 0);
+        }
+
+        [DllImport(Dlls.USER_32, CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool PostMessage(IntPtr windowHandle, uint message, IntPtr wParam, uint lParam);
+    }
+
+}
--- Main.cs
+++ Main.cs
@@ -29,36 +29,6 @@ namespace ThugPro {
         public const int CHARACTER_WAIT_MILLISECONDS = 10;
     }
 
-    class Command {
-        public static void Post(int windowHandle, string message) {
-            ToggleChatBox(windowHandle);
-            TypeMessage(windowHandle, message);
-            ToggleChatBox(windowHandle);
-        }
-
-        private static void ToggleChatBox(int windowHandle) {
-            PressEnter(windowHandle);
-            Thread.Sleep(Timing.CHATBOX_WAIT_MILLISECONDS);
-        }
-
-        private static void PressEnter(int windowHandle) {
-            PostMessage((IntPtr) windowHandle, KeyCodeTypes.WM_KEYDOWN, (IntPtr) KeyCodes.Get("Enter"), 0);
-        }
-
-        private static void TypeMessage(int windowHandle, string message) {
-            foreach (char c in message)
-                PressChar(windowHandle, c);
-        }
-
-        private static void PressChar(int windowHandle, char c) {
-            Thread.Sleep(Timing.CHARACTER_WAIT_MILLISECONDS);
-            PostMessage((IntPtr) windowHandle, KeyCodeTypes.WM_CHAR, new IntPtr((Int32) c), 0);
-        }
-
-        [DllImport(Dlls.USER_32, CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern bool PostMessage(IntPtr windowHandle, uint message, IntPtr wParam, uint lParam);
-    }
-
     class Program {
         static int windowHandle = 0;
         private const int WH_KEYBOARD_LL = 13;
