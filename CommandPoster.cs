0a1114d7eaa4a1d455d2a687622295cdca008753
byxor <brandnewbyxor@gmail.com>
2018-05-28 23:02:20 +0100

    Move Command class

--- CommandPoster.cs
+++ CommandPoster.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
 namespace ThugPro {
 
     class Command {
--- Main.cs
+++ Main.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Diagnostics;
 
 namespace ThugPro {
